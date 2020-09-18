a6d28b0c7e97f86ffcd97d0f8a214375baf4f652
Fredrick Brennan <copypaste@kittens.ph>
2020-09-17 23:51:10 -0700

    Crates.io version 0.1.0
    Don't overwrite src/lib.rs by default anymore; must call as:
    
    DO_BINDGEN=y cargo build

--- build.rs
+++ build.rs
@@ -4,16 +4,42 @@ extern crate bindgen;
 extern crate cc;
 extern crate dos2unix;
 
+use std::env;
 use std::path::PathBuf;
-use std::process::Command;
+
+fn do_bindgen() {
+    // Based on `bindgen` manual.
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .derive_partialeq(true)
+        .derive_debug(true)
+        .derive_copy(true)
+        .derive_default(true)
+        // This function doesn't exist! Struct must be created.
+        .blacklist_function("new_bezctx")
+        // Avoids silly warnings about case of types. They're from C...
+        .raw_line("#![allow(non_camel_case_types, non_snake_case)]")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from("src");
+    let out_file = out_path.join("lib.rs");
+    bindings
+        .write_to_file(out_file.clone())
+        .expect("Couldn't write bindings!");
+
+    // Windows sanity - use \n even on Windows.
+    dos2unix::Dos2Unix::convert(&out_file.into_os_string().into_string().unwrap(), false);
+}
 
 fn main() {
-    // We aren't using libspiro's autotools build, so I wrote a generic spiro-config.h which will
-    // work on all the systems I care about.
-    let cp = Command::new("cp").arg("spiro-config.h").arg("libspiro/").output().unwrap();
-    assert!(cp.status.success());
-
     cc::Build::new()
+        // We aren't using libspiro's autotools build, so I wrote a generic spiro-config.h which
+        // will work on all the systems I care about.
+        .include(".") // for spiro-config.h
         .file("libspiro/bezctx.c")
         .file("libspiro/spiro.c")
         .file("libspiro/spiroentrypoints.c")
@@ -27,30 +53,7 @@ fn main() {
     // Link built library. "lib" and ".a" are added back by Cargo.
     println!("cargo:rustc-link-lib=static=spiro");
 
-    // Below code basically unchanged from `bindgen` manual.
-
-    // The bindgen::Builder is the main entry point
-    // to bindgen, and lets you build up options for
-    // the resulting bindings.
-    let bindings = bindgen::Builder::default()
-        // The input header we would like to generate
-        // bindings for.
-        .header("wrapper.h")
-        // Tell cargo to invalidate the built crate whenever any of the
-        // included header files changed.
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        // Finish the builder and generate the bindings.
-        .generate()
-        // Unwrap the Result and panic on failure.
-        .expect("Unable to generate bindings");
-
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from("src");
-    let out_file = out_path.join("lib.rs");
-    bindings
-        .write_to_file(out_file.clone())
-        .expect("Couldn't write bindings!");
-
-    // Windows sanity - use \n even on Windows.
-    dos2unix::Dos2Unix::convert(&out_file.into_os_string().into_string().unwrap(), false);
+    if env::var("DO_BINDGEN").is_ok() {
+        do_bindgen();
+    }
 }
